f646744307e13a4e060848444c31ffa425376acd
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-04-27 18:11:03 -0500

    switch statement/functions started

--- liri.js
+++ liri.js
@@ -2,9 +2,43 @@ require('dotenv').config();
 
 const keys = require('./keys.js');
 
-const spotify = new Spotify(keys.spotify);
+// const spotify = new Spotify(keys.spotify);
 
 const command = process.argv[2];
 const search = process.argv.slice(3).join(" ");
 
 console.log(search);
+
+function bandsInTownFunc() {
+
+}
+
+function spotifyFunc() {
+
+}
+
+function omdbFunc() {
+
+}
+
+function spotifyFS() {
+
+}
+
+switch (command) {
+  case 'concert-this':
+  bandsInTownFunc(search);
+  break;
+
+  case 'spotify-this-song': 
+  spotifyFunc(search);
+  break;
+
+  case 'movie-this':
+  omdbFunc(search);
+  break;
+
+  case 'do-what-it-says':
+  spotifyFS(search);
+  break;
+}
