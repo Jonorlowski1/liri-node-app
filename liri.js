1212e55e7917a2b27d487af6562bf899afccbffe
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-04-28 17:24:01 -0500

    omdb response logging perfectly

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ const keys = require('./keys.js');
 require('dotenv').config();
 
 
-const spotify = new Spotify(keys.spotify);
+// const spotify = new Spotify(keys.spotify);
 
 const command = process.argv[2];
 const search = process.argv.slice(3).join(" ");
@@ -48,15 +48,15 @@ function omdbFunc() {
     function (response) {
       const data = response.data;
       console.log(lineBreak);
-      console.log(response.data)
-      console.log(chalk`{red.bold Title: }` + data.title)
-      console.log(chalk`{red.bold Year: }` + data.year)
+      // console.log(response.data)
+      console.log(chalk`{red.bold Title: }` + data.Title)
+      console.log(chalk`{red.bold Year: }` + data.Year)
       console.log(chalk`{red.bold IMDB Rating: }` + data.imdbRating)
       // console.log(chalk`{red.bold Rotten Tomatoes Rating: }` + data.ratings[0].value)
-      console.log(chalk`{red.bold Country: }` + data.country)
-      console.log(chalk`{red.bold Language: }` + data.language)
-      console.log(chalk`{red.bold Plot: }` + data.plot)
-      console.log(chalk`{red.bold Actors: }` + data.actors)
+      console.log(chalk`{red.bold Country: }` + data.Country)
+      console.log(chalk`{red.bold Language: }` + data.Language)
+      console.log(chalk`{red.bold Plot: }` + data.Plot)
+      console.log(chalk`{red.bold Actors: }` + data.Actors)
     }
   )
 }
